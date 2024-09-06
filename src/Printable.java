5b953579125640445f46bba61a590528d662ba5d
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-06 20:06:54 +0200

    refactor: int.Printable add printing class name and content

--- src/Printable.java
+++ src/Printable.java
@@ -1,5 +1,19 @@
+import java.lang.reflect.Field;
+
 public interface Printable {
     default void print() {
-        System.out.println("Print content in console");
+        Class<?> oClass = this.getClass();
+        Field[] oFields = oClass.getDeclaredFields();
+
+        System.out.println("Content of " + oClass.getSimpleName() + " is ");
+        for (Field f : oFields) {
+            f.setAccessible(true);
+            try {
+                Object fValue = f.get(this);
+                System.out.println(f.getName() + " - " + fValue);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
