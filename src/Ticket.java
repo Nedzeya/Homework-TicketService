c64f8f059cfa3d40e5bb8ae17035df7f46e36440
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-06 17:34:51 +0200

    refactor: Ticket class

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,5 +1,3 @@
-
-
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -48,33 +46,6 @@ public class Ticket {
         this.time = time;
     }
 
-    private void setId(String id) {
-        if (id == null) {
-            throw new IllegalArgumentException("Identifier cannot be null");
-        } else if (id.length() > 4) {
-            throw new IllegalArgumentException("Identifier must not exceed 4 digits and/or characters.");
-        }
-        this.id = id;
-    }
-
-    private void setConcertHall(String concertHall) {
-        if (concertHall == null) {
-            throw new IllegalArgumentException("Concert hall cannot be null");
-        } else if (concertHall.length() > 10) {
-            throw new IllegalArgumentException("Concert hall must not exceed 10 characters.");
-        }
-        this.concertHall = concertHall;
-    }
-
-    private void setEventCode(String eventCode) {
-        if (eventCode == null) {
-            throw new IllegalArgumentException("Event code cannot be null.");
-        } else if (eventCode.length() != 3 || !eventCode.matches("\\d{3}")) {
-            throw new IllegalArgumentException("Event code must be exactly 3 digits.");
-        }
-        this.eventCode = eventCode;
-    }
-
     public void setTime(long time) {
         this.time = time;
     }
@@ -83,18 +54,17 @@ public class Ticket {
         this.sector = sector;
     }
 
-    //**ability to save ticket price
     public void toSaveTicketPrice(double price) {
         BigDecimal ticketPrice = new BigDecimal(price);
         ticketPrice = ticketPrice.setScale(2, RoundingMode.HALF_UP);
         this.price = ticketPrice;
-          }
-
-    public String printTicketPrice() {
-       return "The ticket id: " + this.id + " price is: " + this.price;
     }
 
-      public String getAllTicketValues() {
+    public String printTicketPrice() {
+        return "The ticket id: " + this.id + " price is: " + this.price;
+    }
+
+    public String getAllTicketValues() {
         Date time = new Date(this.time);
         Date creationTime = new Date(this.creationTime*1000);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -121,7 +91,35 @@ public class Ticket {
     public void shared(String phoneNumber) {
         System.out.println("Ticket was shared by phone to: " + phoneNumber);
     }
+
     public void shared(String phoneNumber, String email) {
         System.out.println("Ticket was shared by phone to: " + phoneNumber + " and by email to: " + email);
+    }
+
+    private void setId(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Identifier cannot be null");
+        } else if (id.length() > 4) {
+            throw new IllegalArgumentException("Identifier must not exceed 4 digits and/or characters.");
+        }
+        this.id = id;
+    }
+
+    private void setConcertHall(String concertHall) {
+        if (concertHall == null) {
+            throw new IllegalArgumentException("Concert hall cannot be null");
+        } else if (concertHall.length() > 10) {
+            throw new IllegalArgumentException("Concert hall must not exceed 10 characters.");
+        }
+        this.concertHall = concertHall;
+    }
+
+    private void setEventCode(String eventCode) {
+        if (eventCode == null) {
+            throw new IllegalArgumentException("Event code cannot be null.");
+        } else if (eventCode.length() != 3 || !eventCode.matches("\\d{3}")) {
+            throw new IllegalArgumentException("Event code must be exactly 3 digits.");
+        }
+        this.eventCode = eventCode;
     }
 }
