4fa30584ac26c6dfd87aee17a3ef4220f2f0abe5
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-09 13:46:13 +0200

    feat: add validateBusTicket method into class BusTicketValidator

---
+++ src/BusTicketValidator.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+
+public class BusTicketValidator {
+
+    public boolean validateBusTicket(BusTicket busTicket) {
+        TicketType ticketType = busTicket.getTicketType();
+        if ((ticketType == TicketType.DAY ||
+                ticketType == TicketType.WEEK ||
+                ticketType == TicketType.YEAR) &&
+                busTicket.getStartDate() <= 0) {
+            System.out.println("Error: Start date is required for ticket type" + ticketType);
+            return false;
+        }
+        if (busTicket.getPrice().compareTo(BigDecimal.ZERO) == 0) {
+            System.out.println("Error: Price can't be zero.");
+            return false;
+        }
+        return true;
+    }
+}
