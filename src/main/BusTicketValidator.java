a259303dfbd9e3c68a8f018029866ddca920109c
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-09 15:17:16 +0200

    feat: add ability to count violations

--- src/main/BusTicketValidator.java
+++ src/main/BusTicketValidator.java
@@ -3,6 +3,8 @@ package main;
 import main.enums.TicketType;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BusTicketValidator {
 
@@ -13,12 +15,20 @@ public class BusTicketValidator {
                 ticketType == TicketType.YEAR) &&
                 busTicket.getStartDate() <= 0) {
             System.out.println("Error: Start date is required for ticket type" + ticketType);
+            addViolationToCounter("start date");
             return false;
         }
         if (busTicket.getPrice().compareTo(BigDecimal.ZERO) == 0) {
             System.out.println("Error: Price can't be zero.");
+            addViolationToCounter("price");
             return false;
         }
         return true;
+    }
+
+    private Map<String, Integer> violationsCounter = new HashMap<>();
+
+    private void addViolationToCounter (String violation) {
+        violationsCounter.put(violation, violationsCounter.getOrDefault(violation, 0) + 1);
     }
 }
