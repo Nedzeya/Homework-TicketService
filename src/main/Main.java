db0479855f9e1b0c5739276ffecf4db2715dbefe
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-12 20:56:37 +0200

    test: add testFineHashSet in Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@ import main.enums.CoinType;
 import main.enums.Sector;
 import main.products.coin.ClientCoinArrayList;
 import main.products.coin.Coin;
+import main.products.fine.Fine;
+import main.products.fine.FineHashSet;
 import main.products.ticket.Ticket;
 import main.products.busTicket.BusTicket;
 import main.products.busTicket.BusTicketReader;
@@ -73,14 +75,37 @@ public class Main {
             busTicketsValidator.validateTickets(busTickets);
 */
             //ClientCoinArrayList testing
+
             testExchangingCoinsForBonus();
+
+            // FineHashSet testing
+            testFineHashSet();
 
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private static void testFineHashSet() {
+        System.out.println("FineHashSet testing.....................");
+        FineHashSet fineSet = new FineHashSet();
+
+        Fine fine1 = new Fine(1, "Traveling without a ticket");
+        Fine fine2 = new Fine(2, "Traveling without a ticket for luggage");
+        Fine fine3 = new Fine(3, "Traveling without a ticket for an animal");
+        Fine fine4 = new Fine(4, "Drunken traveling");
+
+        fineSet.putFine(fine1);
+        fineSet.putFine(fine2);
+        fineSet.putFine(fine3);
+        fineSet.putFine(fine4);
+        System.out.println(fineSet.containsFine(2));
+        fineSet.deleteFine(3);
+        fineSet.iterateFines();
+    }
+
     private static void testExchangingCoinsForBonus() {
+        System.out.println("ClientCoinArrayList testing.....................");
         ClientCoinArrayList coinArrayList = new ClientCoinArrayList();
 
         for (int i = 0; i < 10; i++) {
