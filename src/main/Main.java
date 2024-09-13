64e63c9d11ab7b42053a1be43264b11a8ef54256
Nadzeya <nadzeyailyichyk@gmail.com>
2024-09-13 20:02:35 +0200

    refactor: delete commented out code

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,59 +21,6 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            /*
-            Ticket emptyTicket = createEmptyTicket();
-
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(2024, Calendar.AUGUST, 15, 12, 0, 0);
-            calendar.set(Calendar.MILLISECOND, 0);
-            long eventTime = calendar.getTimeInMillis();
-
-            Ticket fullTicket = createFullTicket(eventTime);
-
-            calendar.set(2024, Calendar.AUGUST, 30, 16, 2, 0);
-            long eventTime1 = calendar.getTimeInMillis();
-
-            Ticket limitedTicket = createLimitedTicket(eventTime1);
-
-// Saving ticket price
-            saveTicketPrice(createEmptyTicket());
-
-            TicketManager ticketManager = new TicketManager();
-//Generating tickets
-            ticketManager.generateTenTickets();
-            //testing getTicketById
-            System.out.println(ticketManager.getTicketById("ID1"));
-            //testing getTicketsByStadiumSector
-            List<Ticket> ticketsByStadiumSector = ticketManager.getTicketsByStadiumSector(Sector.A);
-            System.out.println(ticketsByStadiumSector.size());
-            //testing shared() by phone and by phone and email
-            emptyTicket.shared("123-456-789");
-            fullTicket.shared("123-456-789", "ticket@email.com");
-            //testing users polymorphism
-            AbstractUser client = new Client("Nice Client");
-            AbstractUser admin = new Admin("Good Admin");
-            client.printRole();
-            Ticket clientTicket = ((Client) client).getTicket();
-            admin.printRole();
-            boolean checkingResult = ((Admin) admin).checkTicket(clientTicket);
-            System.out.println("Checking result is: " + checkingResult);
-
-            //testing @NullableWarning
-            Ticket nullSectorFullTicket = new Ticket("ID02",
-                    "Concert",
-                    "001", eventTime,
-                    true,
-                    null,
-                    5.5);
-
-            //BusTicketValidatorTesting and reading from file
-            System.out.println("Read and validate busTickets: ");
-            BusTicketsValidator busTicketsValidator = new BusTicketsValidator();
-            String filePath = "src/main/files/busTickets.json";
-            List<BusTicket> busTickets = BusTicketReader.readTicketsFromFile(filePath);
-            busTicketsValidator.validateTickets(busTickets);
-*/
             //ClientCoinArrayList testing
 
             testExchangingCoinsForBonus();
